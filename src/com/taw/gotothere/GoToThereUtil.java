628175d54c16df2c66d90e9fdb75e670f3eb6e17
Chris Walker <thatamazingweb@gmail.com>
2013-07-09 08:49:23 +0100

    Remove getNoNetworkDialog() method

--- src/com/taw/gotothere/GoToThereUtil.java
+++ src/com/taw/gotothere/GoToThereUtil.java
@@ -23,30 +23,6 @@ public class GoToThereUtil {
 	private static final String ACCEPTED_TOC = "ACCEPTED_TOC";
 	
 	/**
-	 * Return a Network Unavailable dialog for embedding within a fragment.
-	 * @param activity
-	 * @return Dialog constructed dialog
-	 */
-	public static Dialog getNoNetworkDialog(final Activity activity) {
-		return new AlertDialog.Builder(activity)
-			.setTitle(activity.getResources().getString(R.string.no_network_dialog_title))
-			.setMessage(activity.getResources().getString(R.string.no_network_dialog_text))
-			.setNegativeButton(activity.getResources().getString(R.string.settings_button_text), 
-				new DialogInterface.OnClickListener() {
-		           public void onClick(DialogInterface dialog, int id) {
-		        	   activity.startActivity(new Intent(Settings.ACTION_SETTINGS));
-		           }
-		       })
-		    .setPositiveButton(activity.getResources().getString(R.string.ok_button_text), 
-				new DialogInterface.OnClickListener() {
-		           public void onClick(DialogInterface dialog, int id) {
-		        	   activity.finish();
-		           }
-		       })
-			.create();
-	}
-
-	/**
 	 * Return a First Run dialog for embedding within a fragment.
 	 * @param activity
 	 * @return Dialog constructed dialog
